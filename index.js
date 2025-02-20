37a72a508e297edde262c48acdc6c631e42705dc
satyam <58844449+satyam7world@users.noreply.github.com>
2025-02-20 11:32:26 +0530

    boilerplate

---
+++ index.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+const sortedSquares = function (nums) {
+    console.log('sorted square')
+};
+
+
+console.log(sortedSquares([-4, -1, 0, 3, 10]))

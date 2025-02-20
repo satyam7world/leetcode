bd46043e45f333d880eec089e4909d3d02e3b143
satyam <58844449+satyam7world@users.noreply.github.com>
2025-02-20 11:48:58 +0530

    wrote first

--- index.js
+++ index.js
@@ -3,7 +3,23 @@
  * @return {number[]}
  */
 const sortedSquares = function (nums) {
-    console.log('sorted square')
+    console.log(nums)
+    const result = []
+    let lpi = 0;
+    let rpi = nums.length - 1;
+    while (lpi < rpi) {
+        const lpv = Math.pow(nums[lpi], 2);
+        const rpv = Math.pow(nums[rpi], 2);
+        if (lpv > rpv) {
+            result.push(rpv);
+            lpi++
+        } else {
+            result.push(lpv);
+            rpi--
+        }
+        console.log(lpi, rpi)
+    }
+    return result
 };
 
 

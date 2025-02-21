a4f464a66d299884bf840ffbb1549aa565cc29a9
satyam <58844449+satyam7world@users.noreply.github.com>
2025-02-21 16:58:01 +0530

    roman to int

--- index.js
+++ index.js
@@ -1,12 +1,41 @@
 /**
- * @param {character[]} s
- * @return {void} Do not return anything, modify s in-place instead.
+ * @param {string} s
+ * @return {number}
+ * Symbol       Value
+ * I             1
+ * V             5
+ * X             10
+ * L             50
+ * C             100
+ * D             500
+ * M             1000
  */
-let reverseString = function(s) {
+var romanToInt = function (s) {
 
-    return s
+    let result = 0;
+    const map = new Map([
+        ['I', 1],
+        ['V', 5],
+        ['X', 10],
+        ['L', 50],
+        ['C', 100],
+        ['D', 500],
+        ['M', 1000],
+    ]);
+    for (let i = 0; i < s.length; i++) {
+        const character = s[i];
+        const value = map.get(character)
+        const nextValue = map.get(s?.[i + 1]) ?? 0
+        if (nextValue <= value) {
+            result += value
+        } else {
+            result -= value;
+        }
+    }
+    return 'result is ' + result;
 };
 
 
-
-console.log(reverseString(["h","e","l","l","o"]))
+console.log(romanToInt("III"))
+console.log(romanToInt("LVIII"))
+console.log(romanToInt("MCMXCIV"))

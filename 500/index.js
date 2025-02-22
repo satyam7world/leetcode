062779abf36423efb853a6cd543948f9b30ba7e8
satyam <58844449+satyam7world@users.noreply.github.com>
2025-02-22 21:38:58 +0530

    test

--- 500/index.js
+++ 500/index.js
@@ -1,10 +1,30 @@
+// Unsolved
 /**
  * @param {string[]} words
  * @return {string[]}
  */
-var findWords = function(words) {
-
+var findWords = function (words) {
+    const map = new Map();
+    const keyRows = ["qwertyuiop", "asdfghjkl", "zxcvbnm"];
+    for (let word of words) {
+        // const word = '';
+        for (let row of keyRows) {
+            let prev = true;
+            for (let i = 0; i < word.length; i++) {
+                const char = word[i];
+                const included = row.includes(char);
+                if (included && prev) {
+                    map.set(`${word},${row}`, true)
+                } else {
+                    map.set(`${word},${row}`, false)
+                    break
+                }
+                prev = included
+            }
+        }
+    }
+    console.log(map)
 };
 
 
-console.log(findWords(["Hello","Alaska","Dad","Peace"]))
+console.log(findWords(["Hello", "Alaska", "Dad", "Peace"]))

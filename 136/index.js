82f5f0de7b7e097647705b5f0ec3053824913a2b
satyam <58844449+satyam7world@users.noreply.github.com>
2025-02-22 20:04:12 +0530

    mw

--- 136/index.js
+++ 136/index.js
@@ -2,6 +2,18 @@
  * @param {number[]} nums
  * @return {number}
  */
-var singleNumber = function(nums) {
-    
+const singleNumber = function (nums) {
+    const numMap = new Map();
+    for (const num of nums) {
+        if (numMap.has(num)) {
+            numMap.delete(num);
+        } else {
+            numMap.set(num, false);
+        }
+    }
+    return numMap.keys().next().value;
 };
+
+console.log(singleNumber([4, 1, 2, 1, 2]))
+console.log(singleNumber([2, 2, 1]))
+console.log(singleNumber([1]))

cd3a87dd5217bea2c10c7c74c79eb23370cb3ac5
satyam <58844449+satyam7world@users.noreply.github.com>
2025-02-22 12:16:18 +0530

    test

--- index.js
+++ index.js
@@ -1,33 +1,18 @@
 /**
- * @param {number[]} nums
- * @return {number[]}
+ * @param {number[][]} intervals
+ * @return {number[][]}
  */
-var productExceptSelf = function (nums) {
-    const result = []
-    const left = [];
-    let right = [];
-
-    let proLeft = 1;
-    let proRight = 1;
-    for (let i = 0; i < nums.length; i++) {
-        left.push(proLeft)
-        proLeft *= nums[i]
-
-        // right.unshift(proRight);
-        right.push(proRight);
-        proRight *= nums[nums.length - 1 - i]
+const merge = function (intervals) {
+    const result = [];
+    console.log(intervals);
+    for (let i = 1; i < intervals.length; i++) {
+        const [pairF, pairS] = intervals[i - 1];
+        const [currPairF, currPairS] = intervals[i];
+        if (pairF < currPairF < pairS) {
+            result.push([pairF, currPairS])
+        }
     }
-
-    right = right.reverse()
-
-    console.log(left, right)
-
-    for (let i = 0; i < nums.length; i++) {
-        const pro = left[i] * right[i];
-        result.push(pro)
-    }
-
     return result;
 };
 
-console.log(productExceptSelf([1, 2, 3, 4]))
+console.log(merge([[1, 3], [2, 6], [8, 10], [15, 18]]))

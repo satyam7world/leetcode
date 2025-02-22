e3d9db391103af92698230c37518ff7ecefb4c6a
satyam <58844449+satyam7world@users.noreply.github.com>
2025-02-22 20:22:56 +0530

    mw

--- 168/index.js
+++ 168/index.js
@@ -3,5 +3,15 @@
  * @return {string}
  */
 var convertToTitle = function(columnNumber) {
-    
+    const title = '';
+    // const digits = Math.ceil(columnNumber / 26);
+    // console.log(digits, 'dig')
+    //  String.fromCharCode(columnNumber + 64)
+    const xix = parseInt(columnNumber, 26);
+    console.log(String.fromCharCode((xix - 26) + 64))
+    return title;
 };
+
+
+console.log(convertToTitle(1))
+console.log(convertToTitle(27))

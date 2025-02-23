320ea504ecb8458c8d2d67e780ec054de291953a
satyam <58844449+satyam7world@users.noreply.github.com>
2025-02-23 15:06:17 +0530

    mw

---
+++ 1971/index.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} n
+ * @param {number[][]} edges
+ * @param {number} source
+ * @param {number} destination
+ * @return {boolean}
+ */
+const validPath = function (n, edges, source, destination) {
+
+};
+
+console.log(validPath(3, [[0, 1], [1, 2], [2, 0]], 0, 2))

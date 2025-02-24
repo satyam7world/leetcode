3fc1d17d39657b2dee5d2573d0299d33da29bebe
satyam <58844449+satyam7world@users.noreply.github.com>
2025-02-24 10:26:23 +0530

    dfs function

--- 1971/index.js
+++ 1971/index.js
@@ -6,7 +6,28 @@
  * @return {boolean}
  */
 const validPath = function (n, edges, source, destination) {
+    const vis = new Set();
 
+    const g = Array.from({length: n}, () => []);
+    for (const [u, v] of edges) {
+        // console.log(u, v)
+        g[u].push(v);
+        g[v].push(u);
+    }
+
+    const dfs = i => {
+        if (i === destination) {
+            return true;
+        }
+        if (vis.has(i)) {
+            return false;
+        }
+        vis.add(i);
+        return g[i].some(dfs)
+    }
+
+    console.log(edges)
+    console.log(g)
 };
 
 console.log(validPath(3, [[0, 1], [1, 2], [2, 0]], 0, 2))
